b36ffdb09482c1096758c8caa8e68e75c112da9d
Harsh Sharma <harsh.sharma.8986@gmail.com>
2025-07-14 22:53:56 +0530

    feat: add locomotive-scroll

--- src/index.tsx
+++ src/index.tsx
@@ -39,44 +39,39 @@ export default function Home() {
           </h2>
         </div>
 
+        <div className="relative z-30 pb-8 text-md text-white flex flex-col items-center">
+          <span>Scroll Down</span>
+          <ScrollDownComponent />
+        </div>
 
       </section>
-      <div className="relative z-30 pb-8 text-md text-white flex flex-col items-center">
-        <span>Scroll Down</span>
-        <ScrollDownComponent />
-      </div>
-      <section className="bg-black min-h-screen p-8 flex flex-col items-center text-white text-center mt-40">
-        <h2 className="font-poppins text-4xl pb-4">What is Second Brain?</h2>
-        <p className="max-w-3xl text-lg leading-relaxed text-center">
-  <span className="inline-block bg-gradient-to-b from-white to-gray-500 bg-clip-text text-transparent">
-    A Second Brain is a trusted external system for organizing and storing your thoughts, ideas, tasks, and knowledge, so you can think more clearly,
-    create more effectively, and focus on what truly matters.
-    It acts as an extension of your mind, helping you capture insights,
-    reflect with intention, and make better decisions without mental overload.
-  </span>
-</p>
 
-        <div className="flex items-center justify-center p-6">
+      <section className="bg-black min-h-screen p-8 flex flex-col items-center text-white text-center">
+        <h2 className="text-4xl pb-4 pt-20">What is Second Brain?</h2>
+        <p className="max-w-3xl text-lg leading-relaxed text-center">
+          <span className="inline-block bg-gradient-to-b from-white to-gray-500 bg-clip-text text-transparent">
+            A Second Brain is a trusted external system for organizing and storing your thoughts, ideas, tasks, and knowledge, so you can think more clearly,
+            create more effectively, and focus on what truly matters.
+            It acts as an extension of your mind, helping you capture insights,
+            reflect with intention, and make better decisions without mental overload.
+          </span>
+        </p>
+
+        <div className="flex items-center justify-center p-6 mt-8">
           <button className="relative inline-flex h-12 w-36 overflow-hidden rounded-full p-[2px] group cursor-pointer">
             <span
               className="absolute inset-[-200%] animate-[spin_1.6s_linear_infinite] bg-[conic-gradient(from_0deg_at_50%_50%,transparent_0%,rgba(255,255,255,0.9)_100%,transparent_100%)]"
             />
-            <span className="relative inline-flex h-full w-full items-center justify-center rounded-full bg-zinc-900 text-[14px] font-medium text-white backdrop-blur-md transition-all duration-300 ">
+            <span className="relative inline-flex h-full w-full items-center justify-center rounded-full bg-zinc-900 text-[14px] font-medium text-white backdrop-blur-md transition-all duration-300">
               Get Started
             </span>
           </button>
-
-
-
-
-
-
         </div>
       </section>
-      <section className="bg-neutral-950 min-h-screen p-8  text-white">
 
+      <section className="bg-neutral-950 min-h-screen p-8 text-white">
+        <h2 className="text-4xl text-center pt-20">More Content Here</h2>
       </section>
-
 
     </div>
   );
